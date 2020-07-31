27c6fe97c5386ceb761472aca22bd34ee6e19c1d
Renamorcen <renamorcen@gmail.com>
2020-07-31 19:02:57 +0100

    Masofkinas

--- tevas.py
+++ tevas.py
@@ -7,15 +7,22 @@ import time
 testID = 695026179362783344
 realID = 516617064522317835
 pragaroID = 726451417342148618
-svetainesID = 726451417342148622
+svetainesID = 516617064522317839
+mokosiID = 687435655760052403
 
 #realChebra = [118010837335146511,221331058719850497,430000920231804929,170838541855752192,276381693869424640]
 testChebra = [726806470930464768,170838541855752192]
 manaID = 170838541855752192
 tevoID = 726451652520968292
 pijausID = 276381693869424640
+pofkeID = 221331058719850497
+nojausBotID = 738733401460441150
 
-mamosRoleID = 734908204944850974
+
+mamosRoleID = 735595827405389826 
+vaikoRoleID = 735594557344841809
+
+
 
 tekstai = ["Blee-*apsivemia*. Sveiki vakai. Gryzau.",\
         "Kurva mocia ko taip sukauji!?1"]
@@ -33,10 +40,18 @@ class Player():
 
 class Tevas(discord.Client):
     playerlist = []
-    activeID = pragaroID
+    activeID = realID
     mama = None
     svetaineGuild = None
     maxPistisSec = 60
+
+    async def ddos(self):
+        print("Killer")
+        channelis = await self.fetch_channel(mokosiID)
+        while True:
+            await channelis.send("!prideksita niggeris")
+
+
 
     async def pisti(self):
         member = self.svetaineGuild.get_member(self.mama.ID)
@@ -48,7 +63,7 @@ class Tevas(discord.Client):
 
     async def baigti(self):
         member = self.svetaineGuild.get_member(self.mama.ID)
-        roles = []
+        roles = [self.svetaineGuild.get_role(vaikoRoleID)]
         await member.edit(roles = roles)
         channelis = await self.fetch_channel(svetainesID)
         await channelis.send("**AAAAAAAa**")
@@ -60,6 +75,7 @@ class Tevas(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
         await self.init()
+
 
     #bullyinimai
 
@@ -98,16 +114,39 @@ class Tevas(discord.Client):
 
     async def on_message(self, message):
         print(message)
+        print(message.content)
+        if message.content == "!nojau dusk nahuis":
+            channelis = await self.fetch_channel(message.channel.id)
+            await channelis.send("Islaisvinkit broli bota!")
+            await channelis.send("!nojau gali kalbeti")
+
+        if message.content[:11] == "!prideksita" and message.author.bot:
+            await message.delete()
         if message.author.id == manaID and type(message.channel) == discord.DMChannel: #As i pm rasau botui. Cia kad jo komandas handlint.
             print("Brolis raso man.")
+            if message.content == "pisk":
+                kiekPistis = random.random()*self.maxPistisSec
+                await self.pisti()
+                print(kiekPistis)
+                time.sleep(kiekPistis)
+                await self.baigti()
+            if message.content == "ddos":
+                await self.ddos()
+            if message.content == "atpisk mane":
+                member = self.svetaineGuild.get_member(manaID)
+                roles = [self.svetaineGuild.get_role(vaikoRoleID)]
+                await member.edit(roles = roles)
+                
+
 #        print(message.author.id)
 #        print(tevoID)
-        if message.guild.id == self.activeID and message.author.id != tevoID:
+
+        elif message.guild.id == self.activeID and not message.author.bot and not message.author.id == pofkeID and not message.author.id == manaID:# and message.author.id != manaID:
             print("Kazkas sneka.")
             kiekPrisisnekejo = len(message.content)
             player = self.findPlayer(message.author.id)
             if message.author.id == 276381693869424640:
-                player.prisisnekejo += kiekPrisisnekejo*2
+                player.prisisnekejo += kiekPrisisnekejo
             else:
                 player.prisisnekejo += kiekPrisisnekejo
             print(player.prisisnekejo)
@@ -119,6 +158,31 @@ class Tevas(discord.Client):
                 print(kiekPistis)
                 time.sleep(kiekPistis)
                 await self.baigti()
+        if message.author.id == nojausBotID:
+            channelis = await self.fetch_channel(svetainesID)
+            print(message.content)
+            if message.content == "Tevai neuzpisk":
+                await channelis.send("Pisk nx pofke tave uz tokius zodzius iskickins")
+            if message.content == "Jezus":
+                await channelis.send("Marija")
+            if message.content == "Dar jis pabandys kazkam ikist, nebetures su kuo po to.":
+                await channelis.send("Xujist nx")
+            if message.content == "Tevas jau rimtai uzkniso, pastoviai tik kad pagert ir ikist":
+                await channelis.send("Pats ikist negaun")
+            if message.content == "Toks geras zmogus budavai, bet gert pradejai.":
+                await channelis.send(":b:")
+            if message.content == "Supisiu snuki, tevai":
+                await channelis.send("Davai nx *maunasi maike*")
+            if message.content == "Uzdusk pagaliau tevai":
+                await channelis.send("Darba susirask")
+            if message.content == "zmona islaiko tave, nevykeli tu":
+                await channelis.send("mama islaiko tave, nevykeli tu")
+            if message.content == "irgi maunasi maike":
+                await channelis.send("*pisa per pakausi*")
+            if message.content == "pats darba susirask asile":
+                await channelis.send("parduosiu tave cigonam greiciau")
+            if message.content == "...":
+                await channelis.send("kO Ziuri pimpale")
                 
             
 #        print(type(message.channel))
@@ -153,9 +217,11 @@ class Tevas(discord.Client):
                     if nick == None:
                         nick = name
                     self.playerlist += [Player(ID, name, nick)]
+        print(self.playerlist)
         pirmaAukaIndex = random.randint(0,len(self.playerlist)-1)
         self.mama = self.playerlist[pirmaAukaIndex]
         await self.introduce()
+        print("Pirma mama: {0}".format(self.mama.nick))
         print("Init done!")
 
     async def introduce(self):
